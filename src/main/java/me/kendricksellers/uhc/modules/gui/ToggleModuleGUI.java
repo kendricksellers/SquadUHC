0ce0b02772f530e5e8196c8f6a88be85319e8044
Tim Cunningham <timothypaulcunningham@gmail.com>
2021-03-07 17:31:58 -0500

    Only cancle click events if its in a GUI inventory

--- src/main/java/me/kendricksellers/uhc/modules/gui/ToggleModuleGUI.java
+++ src/main/java/me/kendricksellers/uhc/modules/gui/ToggleModuleGUI.java
@@ -54,8 +54,8 @@ public class ToggleModuleGUI extends GUI {
                 }
                 updateInventory();
             }
+            event.setCancelled(true);
         }
-        event.setCancelled(true);
     }
 
     @Override
